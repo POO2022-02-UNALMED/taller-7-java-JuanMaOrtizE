6cf463d02f00982570960760234e460323893839
JuanMaOrtizE <53711761+JuanMaOrtizE@users.noreply.github.com>
2022-10-11 01:42:20 -0500

    :+1:

--- src/main/java/comunicacion/Escrito.java
+++ src/main/java/comunicacion/Escrito.java
@@ -1,35 +1,9 @@
 package comunicacion;
 
-public abstract class Escrito extends Pictograma{
-	
+public abstract class Escrito extends Pictograma {
 	private String titulo;
 	private String autor;
 	private int paginas;
-	public Escrito(String origen, String titulo, String autor, int paginas) {
-		super(origen);
-		this.titulo = titulo;
-		this.autor = autor;
-		this.paginas = paginas;
-	}
-	
-	public String getTitulo() {
-		return titulo;
-	}
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
-	public String getAutor() {
-		return autor;
-	}
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
-	public int getPaginas() {
-		return paginas;
-	}
-	public void setPaginas(int paginas) {
-		this.paginas = paginas;
-	}
 	
 	public String resumen() {
 		String r = this.getOrigen() + "\n";
@@ -39,6 +13,40 @@ public abstract class Escrito extends Pictograma{
 		return  r;
 	}
 	
-	abstract int palabrasTotales(int palabrasPagina);
+	abstract int palabrasTotales(int x);
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutor() {
+		return autor;
+	}
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+	public int getPaginas() {
+		return paginas;
+	}
+
+	public void setPaginas(int paginas) {
+		this.paginas = paginas;
+	}
+
+	public Escrito(String origen, String titulo, String autor, int paginas) {
+		super(origen);
+		this.titulo = titulo;
+		this.autor = autor;
+		this.paginas = paginas;
+	}
+
 	
+	
+
 }
--- src/main/java/comunicacion/Pictograma.java
+++ src/main/java/comunicacion/Pictograma.java
@@ -1,24 +1,24 @@
 package comunicacion;
 
 public abstract class Pictograma {
-	
 	private String origen;
+	
+	abstract String interpretacion();
+	public abstract String toString();
+	
+	public String getOrigen() {
+		return origen;
+	}
+	public void setOrigen(String origen) {
+		this.origen = origen;
+	}
+	
 	
 	public Pictograma(String origen) {
 		super();
 		this.origen = origen;
 	}
-
-	public String getOrigen() {
-		return origen;
-	}
-
-	public void setOrigen(String origen) {
-		this.origen = origen;
-	}
 	
-	abstract String interpretacion ();
-	
-	public abstract String toString();
+
 	
 }
